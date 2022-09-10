0156240b7274d2b6ab283695965ad34784d3465f
Anug Saha <anugsaha@gmail.com>
2022-09-10 15:57:49 -0400

    strart create dog form

--- src/app/components/forms/create-dog/create-dog.component.ts
+++ src/app/components/forms/create-dog/create-dog.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Dog } from 'src/app/dtos/dog';
+import { DogOwner } from 'src/app/dtos/dog-owner';
+import { JwtClientService } from 'src/app/services/jwt-client.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'app-create-dog',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-dog.component.css']
 })
 export class CreateDogComponent implements OnInit {
+  ownerId:number | undefined; 
 
-  constructor() { }
-
-  ngOnInit(): void {
+  createdDog: Dog ={
+    name: '',
+    age: 0,
+    breed: '', 
+    personalities: [], 
+    owner: undefined
   }
 
+  constructor(private jwtService: JwtClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private lsService: LocalStorageService
+  ) { }
+
+  ngOnInit(): void {
+    this.getOwnerId(); 
+  }
+
+  onDogCreate(){ 
+
+  }
+
+  getOwnerId(){ 
+    this.route.paramMap.subscribe(paramMap => {
+      this.createdDog.owner = {id : Number(paramMap.get('id'))};
+    })
+  }
 }
--- src/app/dtos/dog.ts
+++ src/app/dtos/dog.ts
@@ -1,11 +1,11 @@
+import { DogOwner } from "./dog-owner";
 import { Personality } from "./personality";
-import { UserResponseDTO } from "./user-response-dto";
 
 export interface Dog {
-    id:number, 
+    id?:number, 
     name:string, 
     age:number, 
     breed:string, 
-    personalities?: Personality[], 
-    owner?: UserResponseDTO
+    personalities: Personality[], 
+    owner?: DogOwner
 }
